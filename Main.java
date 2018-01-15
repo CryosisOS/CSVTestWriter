2d4767fd7464843e4a8db8afcd022328bb03c705
Nathan van der Velde <33960106+CryosisOS@users.noreply.github.com>
2018-01-15 13:46:07 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/*
+ * Author: Nathan van der Velde
+ * Date Created: 2018-01-15
+ * Last Modified By: Nathan van der Velde
+ * Date Last Modified: 2018-01-15
+ * Description: This is the piece of source code that contains the main method for the program.
+ */
 public class Main
 {
     public static void main(String [] args)
