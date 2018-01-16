4ffe53b1161ef36f0e871f6cc9d9c827302eebea
Nathan van der Valde <nathan@lakesnetworking.local>
2018-01-16 16:35:03 +0800

    Working on a singleton for the Loaded class file.

--- Main.java
+++ Main.java
@@ -5,9 +5,17 @@
  * Date Last Modified: 2018-01-15
  * Description: This is the piece of source code that contains the main method for the program.
  */
+
+//IMPORTS
+import static java.lang.System.out;
+
 public class Main
 {
+    //PRIVATE CLASS FIELDS
+    public static final LoadedSingleton LOADED_FILE;
+
     public static void main(String [] args)
     {
-    }
+        out.print("Hello world!\n");
+    }//END main
 }//END class Main
